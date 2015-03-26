657dffd3ac0904a9bfc621f9e06159347c93e051
Jose Ricardo <jrxavier@gmail.com>
2015-03-25 22:00:57 -0300

    Exemplos do capítulo 1 a 8

--- Exemplo1/Exercicio1.py
+++ Exemplo1/Exercicio1.py
@@ -1,1 +1,35 @@
 __author__ = 'zerix'
+
+
+print("Hello there")
+
+#Entrada de dados para Phyton 3.x
+# value = input('Entre com uma letra: ')
+
+# print(value)
+
+
+
+#Identidade de objetos
+
+nome = 'Jose Ricardo'
+print ( id(nome))
+print (type(nome))
+
+#Mutabilidade dos objetos
+
+# 1 Objetos Imutaveis
+
+idade = 45
+print ("ID do Objeto Inteiro (Idade): " + str(id (idade)))
+idade = idade + 1
+print ("ID do Objeto Inteiro (Idade): " + str(id (idade)))
+
+# 2 Objetos Mutaveis
+
+nomes = []
+print ("ID do Objeto Nomes (Lista): " + str (id (nomes)))
+nomes.append("Jose")
+print ("ID do Objeto Nomes (Lista): " + str (id (nomes)))
+
+
