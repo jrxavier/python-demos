78e3ea864f80871c03a3469b343e4dfab67cf54c
Jose Ricardo <jrxavier@gmail.com>
2015-03-27 22:24:34 -0300

    Detalhamento da secao Objetivos

--- Exemplo1/numberguess.py
+++ Exemplo1/numberguess.py
@@ -1,1 +1,28 @@
 __author__ = 'zerix'
+
+
+import random
+
+def main():
+    smaller = int(input("Enter a small number: "))
+    larger = int(input("Enter a gig number: "))
+
+    myNumber = random.randint(smaller, larger)
+
+    count = 0
+    while True:
+        count+=1
+        userNumber = int (input("Enter you guess: "))
+        if userNumber < myNumber:
+            print("Too small")
+        elif userNumber > myNumber:
+            print("Too bigg")
+        else:
+            print("You've got it in ", count, " tries")
+            break
+
+
+if __name__ == "__main__":
+    main()
+
+
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Python Learning Project
 
 ## Objetivos
+Posibilitar que outras pessoas tenham acesso a exemplos de programas escritos em Python.
+Usarei exemplos básicos e avançados que contarão com estruturas de dados e aplicações matemáticas usando Python
 
 ## Bibliografia utilizada
-
